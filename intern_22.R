a9c6381ad7c805cdf815fe0c25a7378ac9e0f558
Meddy24 <77640544+Meddy24@users.noreply.github.com>
2022-03-25 02:46:39 +0300

    Update intern_22.R

--- intern_22.R
+++ intern_22.R
@@ -1,34 +1,29 @@
+## Load libraries 
 library(tidyverse)
 library(readxl)
-
 library(sysfonts)
 library(extrafont)
 library(bbplot)
 
 showtext::showtext_auto()
 showtext::showtext_opts(dpi=320)
-
 font_families_google()
-
 font_add_google(name="Overpass",family="overpass")
 
+## Import Data Set 
 intern_22_23 <- read_excel("intern - 22-23.xlsx")
 
-
- 
+## Pick major Universities 
 uganda_univer <- c("MUK","KING CEASOR","MUST","KABALE","IUIU","AGA KHAN","BUSITEMA","GULU","KIU",
                    "MUNI","UCU","LIRA") 
-
+## Filter 
 data_intern <- intern_22_23 %>% select(c(3,4,5,7)) %>% 
   filter(UNIVERSITY %in% uganda_univer)
-  
 
+  ## order the universities 
 data_intern$UNIVERSITY <- factor(data_intern$UNIVERSITY, levels = c("KIU", "MUK", "MUST","GULU","LIRA","AGA KHAN","KABALE","IUIU","MUNI","KING CEASOR","UCU"))
 
-parallel <- intern_22_23 %>% count(SEX,QUALIFICATION,UNIVERSITY,sort = TRUE)
-
-head(intern_22_23)
-
+## Plot absolute sex counts 
 p1 <- ggplot(data_intern,aes(UNIVERSITY , fill = SEX)) +
   geom_bar(position = "fill")+ 
   scale_fill_manual(values = c("#B30baa","#287a9c")) +
@@ -37,6 +32,7 @@ p1 <- ggplot(data_intern,aes(UNIVERSITY , fill = SEX)) +
   geom_hline(yintercept = 0.5,color="black",linetype=2)+
   labs(title = " Medical Interns 22-23 | Female - Male (Proportions)")
 
+## Plot sex per course
 p4 <- ggplot(data_intern,aes(QUALIFICATION , fill = SEX)) +
   geom_bar(position="fill") +
   scale_fill_manual(values = c("#B30baa","#287a9c")) +
@@ -45,7 +41,7 @@ p4 <- ggplot(data_intern,aes(QUALIFICATION , fill = SEX)) +
   geom_hline(yintercept = 0.5,color="black",linetype=2)+
   labs(title = " Medical Interns 22-23 | Female - Male (Proportions)")
 
-
+## PLot sex university proportions 
 p2 <- ggplot(data_intern,aes(UNIVERSITY , fill = SEX)) +
   geom_bar() +
   scale_fill_manual(values = c("#B30baa","#287a9c")) +
@@ -53,7 +49,8 @@ p2 <- ggplot(data_intern,aes(UNIVERSITY , fill = SEX)) +
   bbc_style()+
   labs(title = " Medical Interns 22-23 | Female - Male ")
 
-  
+  ## University and Qualifications 
+
 p3 <- ggplot(data_intern,aes(UNIVERSITY,fill = QUALIFICATION)) +
   geom_bar() +
   scale_fill_brewer(palette = "Dark2") +
@@ -61,16 +58,11 @@ p3 <- ggplot(data_intern,aes(UNIVERSITY,fill = QUALIFICATION)) +
    bbc_style()+
   labs(title = " Medical Interns 22-23 | Course")
 
+## Arrange the maps
 map <- grid.arrange(p2,p1,p4,p3)
 
+## save the plot 
 ggsave("intern.jpeg", plot = map, width = 28 , height =  15,dpi=700)
-
-
-
-model <- lm(QUALIFICATION ~ SEX, data = intern_22_23)
-
-
-
 
 
 
